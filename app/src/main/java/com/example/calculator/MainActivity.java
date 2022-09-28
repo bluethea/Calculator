f65593cbde404f8e821446c9282390c0261b16ea
1579132 <nazar.lukiashko@knf.stud.vu.lt>
2022-09-28 10:44:28 +0300

    Created basic button functionality

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button click_button1 = (Button) findViewById(R.id.button1);
+        EditText edit_text = (EditText) findViewById(R.id.edit_text_number);
+        click_button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String retrieved_text = edit_text.getText().toString();
+                if (!TextUtils.isEmpty(retrieved_text)) {
+                    //placeholder
+                }
+            }
+        });
     }
 }
